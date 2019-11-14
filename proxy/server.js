d0f58af0a723c3445552cce373ce40e8cafafca9
Jimmy Ma <jimmyma@Jimmys-MacBook-Pro.local>
2019-11-13 20:58:01 -0800

    proxy stress tested

--- proxy/server.js
+++ proxy/server.js
@@ -1,8 +1,25 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 4000;
+const Axios = require('axios');
+
+var morgan = require('morgan')
+
+app.use(morgan('dev'));
+
 
 app.use('/rooms/:id', express.static(path.join(__dirname, './public')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/api/:id', (req, res) =>{
+    Axios.get('http://localhost:3004/api/:id')
+    .then((data)=>{
+        res.send(data);
+    })
+    .catch((error)=>{
+        res.send('error' + error);
+    })
+});
